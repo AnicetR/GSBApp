26df315747a2ce103dd148cc696b25e84e76e3b5
Anicet REGLAT VIZZAVONA <anicet.reglat@gmail.com>
2016-04-11 00:52:35 +0200

    blabla

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
+ionic.Platform.ready(function() {
+  angular.bootstrap(document, ['starter']);
+});
+
 angular.module('starter', ['ionic', 'restangular', 'ngCordova', 'angular-storage', 'base64'])
 
   // Constant api url
--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -1,6 +1,9 @@
 angular.module('starter')
 
-.controller('AccountCtrl', function($scope, Auth, Frais, store) {
+.controller('AccountCtrl', function($scope, Auth, Frais, store, $cordovaDevice) {
+  var device = $cordovaDevice.getDevice();
+  console.log(device);
+
   $scope.auth = Auth.getSessionInfo();
 
   $scope.updateData = function() {
